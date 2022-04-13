86319f33abda604b3102d5a485104cba0ccbcff8
Chaohui Dong <quic_chaohuid@quicinc.com>
2022-04-13 22:28:43 +0800

    msm: v4l2: add set is_streaming
    fix set is_streaming missing issue
    
    Change-Id: I2804bbb4e4ee30793ad0f37b61702a1df9d6d9d9
    Signed-off-by: Chaohui Dong <quic_chaohuid@quicinc.com>

--- drivers/v4l2loopback-master_v2/v4l2loopback.c
+++ drivers/v4l2loopback-master_v2/v4l2loopback.c
@@ -1933,6 +1933,7 @@ static int vidioc_streamon(struct file *file,
 					msecs_to_jiffies(START_TIMEOUT));
 		if (rc) {
 			rc = data->qcarcam_ctrl_ret;
+			data->is_streaming = 1;
 			if (data->qcarcam_ctrl_ret == 0)
 				dev->stream_status = 1;
 		} else {
