c592a75c55d76156d2a3c4ebb7f374cfd1aba290
Trishansh Bhardwaj <quic_tbhardwa@quicinc.com>
2022-06-06 18:27:35 +0530

    msm: camera: config: enable MMRM for neo
    Enable MMRM for neo target
    
    Change-Id: Ie04fe39ca844dd0e122b239272bf6fcfae7a0816
    Signed-off-by: Trishansh Bhardwaj <quic_tbhardwa@quicinc.com>

--- config/neo.mk
+++ config/neo.mk
@@ -9,3 +9,6 @@ CONFIG_SPECTRA_SENSOR := y
 ccflags-y += -DCONFIG_SPECTRA_ISP=1
 ccflags-y += -DCONFIG_SPECTRA_JPEG=1
 ccflags-y += -DCONFIG_SPECTRA_SENSOR=1
+
+# External Dependencies
+KBUILD_CPPFLAGS += -DCONFIG_MSM_MMRM=1
