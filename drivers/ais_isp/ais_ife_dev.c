3bcd3a523802e918b233f5e7b20436a3e5e8a51f
Liu Jianhua <quic_jianhl@quicinc.com>
2022-06-21 18:01:06 +0530

    msm: ais: Enqueue the discarded frame done event to vfe driver
    The max v4l2 event queue is 30 per IFE HW, which maybe used by
    3-4 RDI interfaces, if ais_server can not dequeue the v4l2 event ASAP,
    then the oldest v4l2 event will merge with the second oldest v4l2 event
    or the oldest v4l2 event will discard.
    If one vfe frame done event discarded, then ais_server and ais driver
    will lost the frame buffer in the frame done event. If ais_server and
    ais driver loses too much frame buffers, then camera will freeze unless
    restart camera application.
    By implementing the v4l2 event merge interface, the oldest frame done
    v4l2 event will enqueued to vfe driver again, just as ais_server
    enqueue an buffer to vfe driver.
    
    CRs-Fixed: 3135126
    Change-Id: Ic9699b4de5a86305e5cd0d7eadf24f8797fabdc1
    Signed-off-by: Liu Jianhua <quic_jianhl@quicinc.com>

--- drivers/ais_isp/ais_ife_dev.c
+++ drivers/ais_isp/ais_ife_dev.c
@@ -26,11 +26,38 @@
 static int ais_ife_driver_cmd(struct ais_ife_dev *p_ife_dev, void *arg);
 static int ais_ife_init_subdev_params(struct ais_ife_dev *p_ife_dev);
 
+void ais_ife_event_merge(const struct v4l2_event *old, struct v4l2_event *new)
+{
+	struct ais_ife_event_data *evt_data = NULL;
+	uint8_t vfe_idx = 0;
+
+	if (old == NULL)
+		return;
+
+	evt_data = (struct ais_ife_event_data *)old->u.data;
+	if ((old->id != V4L_EVENT_ID_AIS_IFE) || (old->type != V4L_EVENT_TYPE_AIS_IFE))
+		return;
+
+	if ((evt_data == NULL) || (evt_data->msg.type != AIS_IFE_MSG_FRAME_DONE))
+		return;
+
+	vfe_idx = evt_data->msg.idx;
+	ais_vfe_discard_old_frame_done_event(vfe_idx, evt_data);
+}
+
+const struct v4l2_subscribed_event_ops ais_ife_subdev_ev_ops = {
+	.add = NULL,
+	.del = NULL,
+	.replace = NULL,
+	.merge = ais_ife_event_merge,  // used to discard old frame done event
+};
+
 static int ais_ife_subdev_subscribe_event(struct v4l2_subdev *sd,
 	struct v4l2_fh *fh,
 	struct v4l2_event_subscription *sub)
 {
-	return v4l2_event_subscribe(fh, sub, AIS_IFE_SUBDEVICE_EVENT_MAX, NULL);
+	return v4l2_event_subscribe(fh, sub, AIS_IFE_SUBDEVICE_EVENT_MAX,
+							&ais_ife_subdev_ev_ops);
 }
 
 static int ais_ife_subdev_unsubscribe_event(struct v4l2_subdev *sd,
