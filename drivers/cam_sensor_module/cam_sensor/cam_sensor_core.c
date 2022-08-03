4b1e0ee7edd584a788b51f14e89e5b41ddc2043a
Waseem Akhtar <quic_wakhtar@quicinc.com>
2022-08-03 17:01:21 +0530

    msm: camera: Camera Marker for KPI measurement.
    Adding Camera Kernel KPI Markers
    
    Change-Id: I6a41ffe5cf7e2631ca34911578d2fae6d82b68ed
    Signed-off-by: Waseem Akhtar <quic_wakhtar@quicinc.com>

--- drivers/cam_sensor_module/cam_sensor/cam_sensor_core.c
+++ drivers/cam_sensor_module/cam_sensor/cam_sensor_core.c
@@ -13,6 +13,8 @@
 #include "cam_common_util.h"
 #include "cam_packet_util.h"
 
+#include <soc/qcom/boot_stats.h>
+
 
 static int cam_sensor_update_req_mgr(
 	struct cam_sensor_ctrl_t *s_ctrl,
@@ -953,6 +955,8 @@ int32_t cam_sensor_driver_cmd(struct cam_sensor_ctrl_t *s_ctrl,
 			"CAM_ACQUIRE_DEV Success, sensor_id:0x%x,sensor_slave_addr:0x%x",
 			s_ctrl->sensordata->slave_info.sensor_id,
 			s_ctrl->sensordata->slave_info.sensor_slave_addr);
+
+		place_marker("M - Hibernation: CAM_ACQUIRE_DEV Success");
 	}
 		break;
 	case CAM_RELEASE_DEV: {
