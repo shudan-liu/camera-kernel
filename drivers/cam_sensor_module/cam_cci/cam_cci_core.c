3909886ea8993a573ac9f9ab33e34308ae8613ce
Liu Jianhua <quic_jianhl@quicinc.com>
2022-08-26 13:56:37 +0530

    msm: ais: change CCI debug log level
    Change CCI debug log from error to debug level.
    
    Change-Id: I4187ec8e784aa536b4a9a6a7f33ac1ecabdf4e13
    Signed-off-by: Liu Jianhua <quic_jianhl@quicinc.com>

--- drivers/cam_sensor_module/cam_cci/cam_cci_core.c
+++ drivers/cam_sensor_module/cam_cci/cam_cci_core.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2017-2021, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -928,7 +929,7 @@ static int32_t cam_cci_data_queue_sync(struct cci_device *cci_dev,
 	cci_dev->cci_wait_sync_cfg.csid = c_ctrl->cfg.cci_wr_sync.sync_cfg.csid;
 	cci_dev->valid_sync = 1;
 
-	CAM_ERR(CAM_CCI, "AIS_SENSOR_I2C_SET_SYNC_PARMS cid %d csid %d",
+	CAM_DBG(CAM_CCI, "AIS_SENSOR_I2C_SET_SYNC_PARMS cid %d csid %d",
 		cci_dev->cci_wait_sync_cfg.csid,
 		c_ctrl->cci_info->id_map);
 
@@ -954,7 +955,7 @@ static int32_t cam_cci_data_queue_sync(struct cci_device *cci_dev,
 			CCI_I2C_M0_Q0_LOAD_DATA_ADDR +	reg_offset, val);
 	cam_io_w_mb(val, base + CCI_I2C_M0_Q0_LOAD_DATA_ADDR + reg_offset);
 
-	CAM_ERR(CAM_CCI, "sid 0x%x cnt %d addr_type %d data_type %d",
+	CAM_DBG(CAM_CCI, "sid 0x%x cnt %d addr_type %d data_type %d",
 		wr_cfg->i2c_config.slave_addr,
 		wr_cfg->count,
 		wr_cfg->addr_type,
@@ -991,7 +992,7 @@ static int32_t cam_cci_data_queue_sync(struct cci_device *cci_dev,
 			cam_io_w_mb(val, base + CCI_I2C_M0_Q0_LOAD_DATA_ADDR
 				+ reg_offset);
 
-			CAM_ERR(CAM_CCI,
+			CAM_DBG(CAM_CCI,
 				"sid 0x%x cnt %d addr_type %d data_type %d",
 				wr_cfg->i2c_config.slave_addr,
 				wr_cfg->count,
@@ -2496,7 +2497,7 @@ static int cam_cci_core_process_write_array_sync_cmd(struct v4l2_subdev *sd,
 	int i;
 	int j;
 
-	CAM_ERR(CAM_CCI, "AIS_SENSOR_I2C_WRITE_ARRAY_SYNC");
+	CAM_DBG(CAM_CCI, "AIS_SENSOR_I2C_WRITE_ARRAY_SYNC");
 
 	rc = copy_from_user(&cci_cmd,
 		(void __user *) cmd->handle,
@@ -2530,7 +2531,7 @@ static int cam_cci_core_process_write_array_sync_cmd(struct v4l2_subdev *sd,
 	cci_ctrl.cfg.cci_wr_sync.num_wr_cfg =
 		cci_cmd.cmd.wr_sync.num_wr_cfg;
 
-	CAM_ERR(CAM_CCI, "write_cmd_cnt %d",
+	CAM_DBG(CAM_CCI, "write_cmd_cnt %d",
 		cci_cmd.cmd.wr_sync.num_wr_cfg);
 
 	for (i = 0; i < cci_cmd.cmd.wr_sync.num_wr_cfg; i++) {
@@ -2700,7 +2701,7 @@ int32_t cam_cci_core_cam_ctrl(struct v4l2_subdev *sd,
 
 		cci_ctrl.cmd = MSM_CCI_SET_SYNC_CID;
 
-		CAM_ERR(CAM_CCI, "AIS_SENSOR_I2C_SET_SYNC_PARMS cid %d csid %d",
+		CAM_DBG(CAM_CCI, "AIS_SENSOR_I2C_SET_SYNC_PARMS cid %d csid %d",
 			cci_ctrl.cfg.cci_wait_sync_cfg.cid,
 			cci_ctrl.cfg.cci_wait_sync_cfg.csid);
 
