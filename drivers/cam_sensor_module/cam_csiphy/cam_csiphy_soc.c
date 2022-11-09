c00bf3a58cd9aebf42a606487b2ddd47d2222538
Tony Lijo Jose <quic_tjose@quicinc.com>
2022-11-09 16:11:00 +0530

    msm: camera: sensor: Fix csiphy 1.2.3 clock vote to dynamic
    Change csiphy clock vote to dynamic vote for rx clock source in case of
    phy hw version 1.2.3.
    
    CRs-Fixed: 3332511
    Change-Id: I0d3c5fef4dc919d9d57f8efd5e90014969f086f4
    Signed-off-by: Tony Lijo Jose <quic_tjose@quicinc.com>

--- drivers/cam_sensor_module/cam_csiphy/cam_csiphy_soc.c
+++ drivers/cam_sensor_module/cam_csiphy/cam_csiphy_soc.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2017-2021, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #include "cam_csiphy_soc.h"
@@ -392,7 +393,7 @@ int32_t cam_csiphy_parse_dt_info(struct platform_device *pdev,
 		csiphy_dev->ctrl_reg->csiphy_common_reg = csiphy_common_reg_1_2_3;
 		csiphy_dev->ctrl_reg->csiphy_reset_enter_regs = csiphy_reset_reg_1_2_3;
 		csiphy_dev->ctrl_reg->csiphy_reset_exit_regs = NULL;
-		csiphy_dev->ctrl_reg->getclockvoting = get_clk_vote_default;
+		csiphy_dev->ctrl_reg->getclockvoting = get_clk_voting_dynamic;
 		csiphy_dev->ctrl_reg->csiphy_reg = csiphy_v1_2_3;
 		csiphy_dev->is_divisor_32_comp = true;
 		csiphy_dev->hw_version = CSIPHY_VERSION_V123;
