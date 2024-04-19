39cb216b2e05deb708fa2ff710661b3144c22d33
Jaydeep Bagthariya <quic_jbagthar@quicinc.com>
2024-04-19 12:55:14 +0530

    msm: camera: common: Fix bazel build error as header not found
    Add kernel based check while inclusion of "linux/dma-iommu.h" file,
    to fix bazel build compilation error.
    
    CRs-Fixed: 3752803
    Change-Id: I82798e7545754fd1e308e58ada443d2dbfa90058
    Signed-off-by: Jaydeep Bagthariya <quic_jbagthar@quicinc.com>

--- drivers/cam_utils/cam_compat.h
+++ drivers/cam_utils/cam_compat.h
@@ -1,7 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /*
  * Copyright (c) 2014-2021, The Linux Foundation. All rights reserved.
- * Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2023-2024 Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #ifndef _CAM_COMPAT_H_
@@ -13,7 +13,10 @@
 #include <linux/iommu.h>
 #include <linux/qcom_scm.h>
 #include <linux/list_sort.h>
+#include <linux/version.h>
+#if (LINUX_VERSION_CODE < KERNEL_VERSION(6, 1, 0))
 #include <linux/dma-iommu.h>
+#endif
 
 #include "cam_csiphy_dev.h"
 #include "cam_cpastop_hw.h"
