2a3c8fc35d5d587f2578a206b1fb5278c9995739
Karthik Dillibabu <quic_kard@quicinc.com>
2024-05-09 18:25:28 +0530

    msm: camera: isp: Handle config dev during flush
    This change handles config dev during flush for
    perport usecase by not allowing config dev for
    other context.
    
    CRs-Fixed: 3821551
    Change-Id: I51c9f1ce75a50cedc941bc2a2355e6b17f1dd030
    Signed-off-by: Karthik Dillibabu <quic_kard@quicinc.com>

--- drivers/cam_isp/cam_isp_context.c
+++ drivers/cam_isp/cam_isp_context.c
@@ -5733,6 +5733,9 @@ static int cam_isp_ctx_flush_all_affected_ctx_stream_grp(
 
 		switch (cmd_type) {
 		case CAM_ISP_CTX_FLUSH_AFFECTED_CTX_SET_FLUSH_IN_PROGRESS:
+			mutex_lock(&ctx_isp->isp_mutex);
+			active_ctx->state = CAM_CTX_FLUSHED;
+			mutex_unlock(&ctx_isp->isp_mutex);
 			atomic_set(&ctx_isp->flush_in_progress, 1);
 			cam_req_mgr_worker_pause(ctx_isp->hw_mgr_worker);
 			cam_req_mgr_worker_flush(ctx_isp->hw_mgr_worker);
@@ -8046,12 +8049,15 @@ static int __cam_isp_ctx_config_dev_in_top_state(
 		if (ctx_isp->acquire_type == CAM_ISP_ACQUIRE_TYPE_VIRTUAL)
 			goto done;
 
+		mutex_lock(&ctx_isp->isp_mutex);
 		if ((ctx->state == CAM_CTX_FLUSHED) || (ctx->state < CAM_CTX_READY)) {
-			rc = -EINVAL;
+			rc = -ECANCELED;
 			CAM_ERR(CAM_ISP, "Received update req %lld in wrong state:%d ctx:%u",
 				req->request_id, ctx->state, ctx->ctx_id);
+			mutex_unlock(&ctx_isp->isp_mutex);
 			goto put_ref;
 		}
+		mutex_unlock(&ctx_isp->isp_mutex);
 
 		if (ctx_isp->offline_context) {
 			__cam_isp_ctx_enqueue_request_in_order(ctx, req);
@@ -9046,10 +9052,12 @@ static int __cam_isp_ctx_start_dev_in_ready(struct cam_context *ctx,
 	start_isp.is_trigger_type =
 		(ctx_isp->stream_type == CAM_REQ_MGR_LINK_TRIGGER_TYPE) ? true : false;
 
+	mutex_lock(&ctx_isp->isp_mutex);
 	if (ctx->state == CAM_CTX_FLUSHED)
 		start_isp.start_only = true;
 	else
 		start_isp.start_only = false;
+	mutex_unlock(&ctx_isp->isp_mutex);
 
 	__cam_isp_context_reset_ctx_params(ctx_isp);
        if (ctx_isp->acquire_type == CAM_ISP_ACQUIRE_TYPE_VIRTUAL)
