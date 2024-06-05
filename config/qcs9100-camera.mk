09f999104ddc282016de781312b32629c611dc1b
Stark Lin <quic_starlin@quicinc.com>
2024-06-05 19:15:02 +0800

    msm: camera: sensor: Enable sensor compilation for qcs9100
    This change enables sensor compilation for qcs9100.
    
    CRs-Fixed: 3835504
    Change-Id: Id6ed539d3caa9ace84d640541eeb660fff868fee
    Signed-off-by: Stark Lin <quic_starlin@quicinc.com>

--- config/qcs9100-camera.mk
+++ config/qcs9100-camera.mk
@@ -3,12 +3,12 @@
 # Localized KCONFIG settings
 CONFIG_SPECTRA_ISP := y
 CONFIG_SPECTRA_ICP := y
-# CONFIG_SPECTRA_SENSOR := y
+CONFIG_SPECTRA_SENSOR := y
 
 # Flags to pass into C preprocessor
 ccflags-y += -DCONFIG_SPECTRA_ISP=1
 ccflags-y += -DCONFIG_SPECTRA_ICP=1
-# ccflags-y += -DCONFIG_SPECTRA_SENSOR=1
+ccflags-y += -DCONFIG_SPECTRA_SENSOR=1
 
 # External Dependencies
 # KBUILD_CPPFLAGS += -DCONFIG_MSM_MMRM=1
